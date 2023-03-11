40516d021d119faf074085db183f3acb8d338297
Hosam H <harooubi@gmail.com>
2023-03-11 18:06:00 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name = 'Ralph') {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const r1 = cats.slice()
+    r1.push(name)
+    return r1
+}
+function prependCat(name) {
+    const r1 = cats.slice()
+    r1.unshift(name)
+    return r1
+}
+function removeLastCat() {
+    const r2 = cats.slice()
+    r2.pop()
+    return r2
+}
+function removeFirstCat() {
+    const r3 = cats.slice()
+    r3.shift()
+    return r3
+}
